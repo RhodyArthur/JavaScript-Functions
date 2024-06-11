cc66033cec1ca308c0b357dcde12c78ed5d62c2f
Rhoda Arthur <117458137+RhodyArthur@users.noreply.github.com>
2024-06-11 16:59:21 +0000

    calculates the average of numbers

--- script.js
+++ script.js
@@ -58,3 +58,13 @@ function sum(arr) {
   }
   return total;
 }
+
+// d. calculates the average
+function average(arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  return total / arr.length;
+}
+console.log(average(arr));
