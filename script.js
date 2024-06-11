fd2f0488b1bd726bcc8f338c971608b96f99a251
Rhoda Arthur <117458137+RhodyArthur@users.noreply.github.com>
2024-06-11 15:54:22 +0000

    capitalizes the first letter of a string

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
-//reverses a string
+// String Transformations
+// a. Capitalizes the first letter of a string
+function capitalize(str) {
+  return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+
+//b. reverses a string
 function reverse(str) {
-  reversedString = "";
+  let reversedString = "";
   for (let i = str.length - 1; i >= 0; i--) {
     reversedString += str[i];
   }
