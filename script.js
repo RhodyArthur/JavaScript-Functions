3e77e98f8b7498a69da4ae91062799955971e452
Rhoda Arthur <117458137+RhodyArthur@users.noreply.github.com>
2024-06-11 15:43:13 +0000

    a function to reverse a string

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+//reverses a string
+function reverse(str) {
+  reversedString = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedString += str[i];
+  }
+  return reversedString;
+}
